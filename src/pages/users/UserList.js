3f00c17986b55b7d4b7e49502c6f7bf4e5d249b3
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-09-04 16:25:18 +0530

    Users List

--- src/pages/users/UserList.js
+++ src/pages/users/UserList.js
@@ -2,7 +2,30 @@ import React from 'react'
 import MainLayout from '../../layouts/MainLayout'
 
 const UserList = () => {
-  return <MainLayout>User Listing</MainLayout>
+  return (
+    <MainLayout>
+      <section className='panel'>
+        <h2>User Table</h2>
+
+        <table className='table'>
+          <tr>
+            <th>S.No</th>
+            <th>ID</th>
+            <th>Created Date (dd-mm-yyyy)</th>
+            <th>Name</th>
+            <th>Image</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Germany</td>
+            <td>Germany</td>
+          </tr>
+        </table>
+      </section>
+    </MainLayout>
+  )
 }
 
 export default UserList
